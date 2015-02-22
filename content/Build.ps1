bd0c561fdb5dcbc3699039b2b0fc8f58f55c3593
Duane Edwards <mail@duaneedwards.net>
2015-02-22 20:24:17 +1000

    update build / deploy / run targets to be powershell scripts

--- content/Build.ps1
+++ content/Build.ps1
@@ -1,1 +1,2 @@
-powershell -Command "& {Import-Module .\packages\psake.4.4.1\tools\psake.psm1; Invoke-psake .\default.ps1 Build %*}"
+Import-Module .\packages\psake.4.4.1\tools\psake.psm1
+Invoke-psake .\default.ps1 Build
